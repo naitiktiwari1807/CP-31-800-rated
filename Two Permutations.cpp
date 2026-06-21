73097ebcc868342699863413d29835b51422edbf
naitiktiwari1807 <naitik.tiwari0718@gmail.com>
2026-06-21 10:52:20 +0530

    Update Two Permutations.cpp

---
+++ Two Permutations.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,a,b;
+        cin>>n>>a>>b;
+        if((a+b+2<=n)||(a==n&&a==b)){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+       
+    }
+    return 0;
+}
